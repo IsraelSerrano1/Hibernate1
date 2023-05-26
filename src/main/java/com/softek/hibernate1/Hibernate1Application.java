cab69542e7eee422445dba6a7ca25c4d0d334f05
IsraelSerrano1 <israel.serrano@softtek.com>
2023-05-26 08:02:51 +0200

    cambios esteticos de respuestas

--- src/main/java/com/softek/hibernate1/Hibernate1Application.java
+++ src/main/java/com/softek/hibernate1/Hibernate1Application.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Hibernate1Application implements CommandLineRunner {
+
     @Autowired
     private IPilotoService pServicio;
     public static void main(String[] args) {
